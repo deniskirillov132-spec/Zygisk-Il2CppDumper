9b99c1ab29ac62cd5cff61f48020cad3f1100602
deniskirillov132-spec <deniskirillov132@gmail.com>
2026-03-12 20:00:41 +0300

    game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,7 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
+#define DUMP_DELAY 10000
 #define GamePackageName "com.game.packagename"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
